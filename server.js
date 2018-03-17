e39f9703ab71c264e03dc35b99173e7cc990993f
Amnah Droobi <amnahdroobi@hotmail.com>
2018-03-18 01:14:25 +0200

    read drugs file from the server from the listeninig port

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const fs = require('fs');
 var bodyParser = require('body-parser');
 let firstFile = fs.readFileSync('./users.json', 'utf-8');
 let users = JSON.parse(firstFile);
-
+let secondFile = fs.readFileSync('./drug.json', 'utf-8');
+let drugs = JSON.parse(secondFile);
 
 app.get('/users', (req, res) => res.json(users)
 );
 
+app.get('/drugs', (req, res) => res.json(drugs)
+);
 
 
 
