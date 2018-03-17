cc2dfc396a097c92d31228498b2ace10cf1073ed
Amnah Droobi <amnahdroobi@hotmail.com>
2018-03-18 01:13:10 +0200

    open users file from the server from the listening port

--- server.js
+++ server.js
@@ -4,3 +4,22 @@ const fs = require('fs');
 var bodyParser = require('body-parser');
 let firstFile = fs.readFileSync('./users.json', 'utf-8');
 let users = JSON.parse(firstFile);
+
+
+app.get('/users', (req, res) => res.json(users)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => console.log("done"));
